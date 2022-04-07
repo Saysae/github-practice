09db4589ceafd547faf360833e000dcd399dbad9
Patrick Luong <pluonggr@srv603.webhostingforstudents.com>
2022-04-07 12:55:39 -0400

    testing global credential helper

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ echo"Hi I am behind in class this is my first commit change";
 echo "this is my 2nd add for practice!";
 
 echo "this is my third practice";
+
+echo"testing git config --global credntial.helper store";
 ?>
 

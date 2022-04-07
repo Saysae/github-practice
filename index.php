d9a03cf1205574c09439fdff27548c4d2434a0a1
Patrick Luong <pluonggr@srv603.webhostingforstudents.com>
2022-04-07 12:51:44 -0400

    i made another mistake...

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo"Hi I am behind in class this is my first commit change";
 
-echo "this is my 2nd add for practice!"
+echo "this is my 2nd add for practice!";
 
 echo "this is my third practice";
 ?>
